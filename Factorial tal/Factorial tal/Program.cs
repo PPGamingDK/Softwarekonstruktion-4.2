4a15eca3e3c3f1fbe1b40c1ad1832f5d4ba29788
PPGamingDK <45538799+PPGamingDK@users.noreply.github.com>
2023-03-20 10:37:11 +0100

    Factorial tal.

---
+++ Factorial tal/Factorial tal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Indtast et tal: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Factorial af {0} er {1}", num, Factorial(num));
+
+
+static int Factorial(int num)
+{
+    if (num == 0)
+        return 1;
+    else
+        return num * Factorial(num - 1);
+}
